8248cf775869aa83c1eec173274ee3b245565867
ProxPxD <piotr10tutek@poczta.onet.pl>
2021-02-25 20:12:06 +0100

    last part

---
+++ tablas-v3/TEMP_V3.sql
@@ -0,0 +1,20 @@
+USE [SNTBROKERv2]
+GO
+
+/****** Object:  Table [dbo].[TEMP_V2]    Script Date: 24/02/2021 20:42:31 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TEMP_V3](
+	[MATRIZ1] [char](78) NOT NULL
+) ON [PRIMARY]
+GO
+
+
+INSERT INTO [dbo].[TEMP_V3]
+select
+	convert(char(78), MATRIZ1) as MATRIZ1
+from [SNTBROKER_SIE].[dbo].[TEMP_V2]
---
+++ tablas-v3/ULTDIF_ARBOLDECISION_TEMP_V3.sql
@@ -0,0 +1,27 @@
+USE [SNTBROKERv2]
+GO
+
+/****** Object:  Table [dbo].[ULTDIF_ARBOLDECISION_TEMP_v2]    Script Date: 24/02/2021 21:40:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ULTDIF_ARBOLDECISION_TEMP_v3](
+	[IDEMPRESA] [smallint] NOT NULL,
+	[CLAVE] [varchar](5) NOT NULL,
+	[IDPATRON] [varchar](15) NOT NULL,
+	[NUMERO] [smallint] NOT NULL
+) ON [PRIMARY]
+GO
+
+INSERT INTO [dbo].[ULTDIF_ARBOLDECISION_TEMP_v3]
+select
+	convert(smallint, IDEMPRESA) AS IDEMPRESA,
+	convert(varchar(5), CLAVE) AS CLAVE,
+	convert(varchar(15), IDPATRON) AS IDPATRON,
+	convert(smallint, convert(real, NUMERO)) AS NUMERO
+from [SNTBROKER_SIE].[dbo].[ULTDIF_ARBOLDECISION_TEMP_v2]
+
+
